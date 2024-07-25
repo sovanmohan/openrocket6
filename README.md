721a823ce82a7e8cd195e88eae2200c235786621
sovanmohan <msovan928@gmail.com>
2024-07-25 13:29:28 +0530

    Commit message goes here

---
+++ README.md
@@ -0,0 +1,9 @@
+# Openrocket6
+
+{{ appDescription }}
+
+[Releases](https://github.com/sovanmohan/openrocket6/releases)
+
+## About this Project
+
+This project was generated using the Swing project template from [jDeploy](https://www.jdeploy.com), a tool to deploy Java desktop apps on Windows, Mac.
---
+++ src/test/java/com/github/sovanmohan/openrocket6/Openrocket6Test.java
@@ -0,0 +1,38 @@
+package com.github.sovanmohan.openrocket6;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple App.
+ */
+public class Openrocket6Test
+    extends TestCase
+{
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public Openrocket6Test( String testName )
+    {
+        super( testName );
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite()
+    {
+        return new TestSuite( Openrocket6Test.class);
+    }
+
+    /**
+     * Rigourous Test :-)
+     */
+    public void testApp()
+    {
+        assertTrue( true );
+    }
+}
